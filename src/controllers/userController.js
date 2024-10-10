0e216e3ba8450577195d12eecc8af1799ae977b5
Aris Arisandi <idnasirasira@gmail.com>
2024-10-10 10:19:14 +0700

    Add user controller

---
+++ src/controllers/userController.js
@@ -0,0 +1,55 @@
+const userService = require('../services/userService');
+
+exports.getAllUsers = async (req, res, next) => {
+  try {
+    const users = await userService.getAllUsers();
+    res.json(users);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getUserById = async (req, res, next) => {
+  try {
+    const user = await userService.getUserById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.createUser = async (req, res, next) => {
+  try {
+    const newUser = await userService.createUser(req.body);
+    res.status(201).json(newUser);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.updateUser = async (req, res, next) => {
+  try {
+    const updatedUser = await userService.updateUser(req.params.id, req.body);
+    if (!updatedUser) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.json(updatedUser);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.deleteUser = async (req, res, next) => {
+  try {
+    const deleted = await userService.deleteUser(req.params.id);
+    if (!deleted) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.status(204).end();
+  } catch (error) {
+    next(error);
+  }
+};
