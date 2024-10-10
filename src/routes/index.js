8ae0aae8a4454c7b0d342d3e3bf057fc3415effb
Aris Arisandi <idnasirasira@gmail.com>
2024-10-10 10:21:32 +0700

    Add user routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const userRouter = require('./userRoutes');
 
 router.get('/', (req, res) => {
   res.status(200).json({
@@ -8,5 +9,7 @@ router.get('/', (req, res) => {
     version: '1.0.0',
   });
 });
+
+router.use('/users', userRouter);
 
 module.exports = router;
